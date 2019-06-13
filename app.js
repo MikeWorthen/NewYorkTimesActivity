e202c11a60a4c68932538b3431460a67f17e9d92
Tyler Nichols <tegulator98@gmail.com>
2019-06-12 21:30:58 -0600

    click function made trying to fix bugs.

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 var apiUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news-desk('Foreign')&api-key=dg96Vx4FphtgeR2YjncVABWT3LnGyzdn";
- console.log(apiUrl);
+
  $.ajax({
     url: apiUrl,
     method: "GET"
 }).then(function(response){
 console.log(response);
-var searchTerm = $("#search-term")
+var searchTerm = $("search-term")
 var option1 = $("#num-records-select")
 var startYear = $("#start-year")
 var endYear = $("#end-year")
+
+   
+    $("#run-search").on("click",function(){
+        apiUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq="+ searchTerm +"&api-key=dg96Vx4FphtgeR2YjncVABWT3LnGyzdn";
+        console.log(searchTerm);
+        console.log(option1);
+        console.log(startYear);
+        console.log(endYear);
+        console.log(apiUrl);
+        debugger;
+});
+
 });
