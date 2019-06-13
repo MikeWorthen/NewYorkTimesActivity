451f018b44c32b2f30d1e2f69e8194f73e1aa905
Tyler Nichols <tegulator98@gmail.com>
2019-06-12 21:11:39 -0600

    added variables for input fields.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-var apiUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=weather&api-key=dg96Vx4FphtgeR2YjncVABWT3LnGyzdn";
+var apiUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news-desk('Foreign')&api-key=dg96Vx4FphtgeR2YjncVABWT3LnGyzdn";
  console.log(apiUrl);
  $.ajax({
     url: apiUrl,
     method: "GET"
 }).then(function(response){
 console.log(response);
+var searchTerm = $("#search-term")
+var option1 = $("#num-records-select")
+var startYear = $("#start-year")
+var endYear = $("#end-year")
 });
