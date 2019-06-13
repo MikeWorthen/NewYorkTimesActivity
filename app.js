27cef94a5e695fc2bf1ac00f394ad5e32962ff60
Tyler Nichols <tegulator98@gmail.com>
2019-06-12 21:39:59 -0600

    Fixed a bug.

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-var apiUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news-desk('Foreign')&api-key=dg96Vx4FphtgeR2YjncVABWT3LnGyzdn";
+var apiUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=dg96Vx4FphtgeR2YjncVABWT3LnGyzdn&q=";
 
  $.ajax({
     url: apiUrl,
     method: "GET"
 }).then(function(response){
 console.log(response);
-var searchTerm = $("search-term")
-var option1 = $("#num-records-select")
-var startYear = $("#start-year")
-var endYear = $("#end-year")
+
 
    
-    $("#run-search").on("click",function(){
-        apiUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq="+ searchTerm +"&api-key=dg96Vx4FphtgeR2YjncVABWT3LnGyzdn";
+   
+
+});
+ $("#run-search").on("click",function(){
+        debugger;
+        var searchTerm = $("#search-term").val().trim();
+        var searchUrl = apiUrl + searchTerm;
+var option1 = $("#num-records-select");
+var startYear = $("#start-year");
+var endYear = $("#end-year");
         console.log(searchTerm);
+        console.log(searchUrl);
         console.log(option1);
         console.log(startYear);
         console.log(endYear);
         console.log(apiUrl);
         debugger;
-});
-
 });
