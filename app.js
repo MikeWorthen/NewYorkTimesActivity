562ed956629b3a150ba0012add8bc18821417302
Tyler Nichols <tegulator98@gmail.com>
2019-06-12 20:56:00 -0600

    Created AJAX and logged it.

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-var apiURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=weather&api-key=dg96Vx4FphtgeR2YjncVABWT3LnGyzdn";
- console.log(apiURL);
+var apiUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=weather&api-key=dg96Vx4FphtgeR2YjncVABWT3LnGyzdn";
+ console.log(apiUrl);
+ $.ajax({
+    url: apiUrl,
+    method: "GET"
+}).then(function(response){
+console.log(response);
+});
